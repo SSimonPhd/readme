64276ede33964181f31e1253439782e650341b72
SSimonPhd <simon_phd@protonmail.com>
2022-09-28 02:06:33 -0700

    Initial JS code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const getLicense = require("./utils/getLicense");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	{
+    type: "input",
+    name: "title",
+    message: "Project title:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Project description:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Usage information:",
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "Contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Test instructions:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Select license for application:",
+    choices: ["Apache", "MIT", "GNU GPLv3"],
+  },
+  {
+    type: "input",
+    name: "git",
+    message: "GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Your email address:",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(error, "Resubmit.");
+    }
+  });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers, "Success!  README.md generated");
+    const licenseInfo = getLicense(answers);
+    writeToFile("createdReadMe.md", generateMarkdown(answers, licenseInfo));
+  });
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,52 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function to generate markdown
+function generateMarkdown(answers, licenseInfo) {
+  return `# ${answers.title}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+![badge](https://img.shields.io/badge/License-${answers.license}-blue)
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+## Table of Contents
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributions](#contributions)
+- [Tests](#tests)
+- [Questions](#questions)
 
-`;
+## Description
+
+  ${answers.description}
+
+## Installation
+
+  ${answers.installation}
+
+## Usage
+
+  ${answers.usage}
+
+## License
+
+  ![badge](https://img.shields.io/badge/License-${answers.license}-blue)
+
+  ${licenseInfo}
+
+## Contributions
+
+GitHub Profile of Contributors:[${answers.contributions}](https://github.com/${answers.contributions}/)
+
+## Tests
+
+  ${answers.tests}
+
+## Questions
+
+  Have questions or feedback?\n
+  Email:${answers.email}\n
+  Let's connect:\n
+  GitHub Profile:[${answers.git}](https://github.com/${answers.git}/)\n
+  GitHub Repository:[${answers.git}](https://github.com/${answers.git}/)
+  `;
 }
-
 module.exports = generateMarkdown;
